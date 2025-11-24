907c78e41c2d75b13ce7e1ac3e48863bcb10e0b7
emilio <emilio3bolivar3@gmail.com>
2025-11-24 15:16:59 -0300

    tipografia

--- comunidadfront/src/app/layout.tsx
+++ comunidadfront/src/app/layout.tsx
@@ -1,16 +1,19 @@
 // app/layout.tsx (Server Component)
 import "./globals.css";
 import Navbar from "./Inicio/components/Navbar";
-
+import { Itim } from 'next/font/google';
 export const metadata = {
   title: "Comunidad Solidaria",
   description: "Login",
 };
-
+const itim = Itim({
+  weight: '400',
+  subsets: ['latin'],
+});
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body>
+      <body className={itim.className}>
         <Navbar />
         {children}
       </body>
